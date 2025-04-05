00ed4d6d466076ed71a093ae9950b5c31439e7d9
William Galvão <Liam_draven@outlook.com>
2025-04-05 14:03:09 -0300

    Update ProcessoSeletivo.java

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,11 +1,41 @@
 package candidatura;
 
+import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class ProcessoSeletivo {
     // Programa pricipal
     public static void main(String[] args) {
         selecaoCandidatos();
+    }
+
+    static void entrandoEmContato(String candidato){
+        int tentativaRealizadas = 1;
+        boolean continuarTentando = true;
+        boolean atendeu = false;
+        do{
+            atendeu= atender();
+            continuarTentando = !atendeu;
+            if (continuarTentando){
+                tentativaRealizadas++;
+            }else {
+                System.out.println("CONTATO REALIZADO COM SUCESSO");
+            }
+        }while(continuarTentando && tentativaRealizadas<3);
+    }
+
+    // Metodo atender
+    static boolean atender(){
+        return new Random().nextInt(3)==1;
+    }
+
+    // Metodo para imprimir selecionados
+    static void imprimirSelecionados(){
+        String [] candidatos = {"Felipe","Marcia","Julia","Paulo","Augusto","Monica","Fabrício"};
+        System.out.println("Imprimindo a lista de candidatos ");
+        for (int i = 0; i < candidatos.length ; i++) {
+            System.out.println("O candidato de nª "+(i+1)+" é "+candidatos[i]);
+        }
     }
 
     // Metodo de seleção do candidato
